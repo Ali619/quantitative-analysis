604cde068a7d7a08d2eb0c428de3a5395ef2dbc4
ali619 <a.bahadorani@outlook.com>
2024-12-08 01:03:23 +0330

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from crawlers.ExchangeCrawler import exchange_fetch_price
-from sqlite.Sqlite import add_to_db
 import pandas as pd
 
-if __name__ == "__main__":
-    df = exchange_fetch_price(timeframe='4h', symbol='BTC/USDT')
-    # df = pd.read_csv('./exchange-fetched-data/2024-12-07_20-17-4H-data.csv')
-    # add_to_db(df=df, database_name='coinex_4h', table_name='BTC_USDT')
+from crawlers.exchangeCrawler import exchange_fetch_price
+from sqlite.sqlite import add_to_db
 
+if __name__ == "__main__":
+    df = exchange_fetch_price(timeframe="4h", symbol="BTC/USDT")
+    # df = pd.read_csv('./exchange-fetched-data/2024-12-07_22-24-4H-data.csv')
+    add_to_db(df=df, database_name="coinex_4h", table_name="BTC_USDT")
