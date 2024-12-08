a714441bbdfe750ace8b0196623dd386035c89e7
ali619 <a.bahadorani@outlook.com>
2024-12-08 18:21:11 +0330

    Modify functions to fetch data asyncronos. Also changed some prints output

--- crawlers/exchangeCrawler.py
+++ crawlers/exchangeCrawler.py
@@ -1,10 +1,15 @@
-import ccxt
-import pandas as pd
-from dotenv import load_dotenv
-import time
+import asyncio
 import datetime
 import os
+import time
 from pathlib import Path
+
+import ccxt.async_support as ccxt
+import pandas as pd
+from dotenv import load_dotenv
+
+# import ccxt
+
 
 NOW = datetime.datetime.now()
 YEAR = datetime.datetime.now().year
@@ -17,12 +22,12 @@ ID = os.getenv("id")
 SECRET = os.getenv("secret")
 
 
-def exchange_fetch_price(
+async def exchange_fetch_price(
     timeframe: str, symbol: str, limit: int = 1000
 ) -> pd.DataFrame:
     """Fetch data from exchange from `2023-01-01T00:00:00Z`. Default exchange is CoinEx and it's not changeble at this time.
     params:
-        `timeframe`: timeframe of data
+        `timeframe`: timeframe of data (for e.x. `"4h"`)
         `limit`: number of data to fetch
         `symbol`: symbol of data (for e.x. `"BTC/USDT"`)
     """
@@ -37,9 +42,10 @@ def exchange_fetch_price(
     since = exchange.parse8601(since)
     for retries in range(5):
         try:
-            ohlcv = exchange.fetch_ohlcv(
+            ohlcv = await exchange.fetch_ohlcv(
                 symbol=symbol, timeframe=timeframe, since=since, limit=limit
             )
+            await exchange.close()
             break
         except (
             ccxt.ExchangeError,
@@ -49,16 +55,30 @@ def exchange_fetch_price(
             ccxt.ExchangeNotAvailable,
             Exception,
         ) as e:
-            print(type(e).__name__, e)
-            time.sleep(retries + 1)
+            print(type(e).__name__, e, f"waiting for {retries + 1} secs")
+
+            await asyncio.sleep(retries + 1)
     else:
         print("Maximum retries reached, fetching data is closed.")
         exit()
+    file_name = f'{PATH}/{symbol.replace("/", "_")}-{timeframe}-{TIME}'
     print(
-        f"ohlvc data is fetched. Dataframe created and stored in /exchange-fetched-data/{
-          TIME}-{timeframe.upper()}-data.csv"
+        f"ohlvc for {symbol} is fetched, dataframe created and stored in {file_name}.csv"
     )
     df = pd.DataFrame(ohlcv, columns=["date", "open", "high", "low", "close", "volume"])
     df["date"] = pd.to_datetime(df["date"], unit="ms")
-    df.to_csv(f"{PATH}/{TIME}-{timeframe.upper()}-data.csv", index=False)
+    df.to_csv(
+        f"{file_name}.csv",
+        index=False,
+    )
     return df
+
+
+async def run_asynco_fetch(symbols, timeframes):
+    task = [
+        exchange_fetch_price(timeframe=timeframe, symbol=symbol)
+        for symbol in symbols
+        for timeframe in timeframes
+    ]
+    result = await asyncio.gather(*task)
+    return result
