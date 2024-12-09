e2d941e1515e068652127f052255d79febd086dc
ali619 <a.bahadorani@outlook.com>
2024-12-09 12:09:53 +0330

    add async functions to create dbs and add data to tables

--- sqlite/sqlite.py
+++ sqlite/sqlite.py
@@ -1,4 +1,6 @@
+import asyncio
 import sqlite3
+from glob import glob
 from pathlib import Path
 
 import pandas as pd
@@ -13,7 +15,7 @@ def create_table(datanase_name: str, table_name: str) -> None:
     cursor = conn.cursor()
     cursor.execute(
         f"""
-                CREATE TABLE IF NOT EXISTS {table_name} (
+                CREATE TABLE IF NOT EXISTS "{table_name}" (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     date DATE,
                     open REAL,
@@ -28,13 +30,11 @@ def create_table(datanase_name: str, table_name: str) -> None:
     conn.close()
 
 
-def add_to_db(
-    df: pd.DataFrame, database_name: str, table_name: str, based_on: str = "date"
-) -> None:
+def add_to_db(df: pd.DataFrame, database_name: str, table_name: str) -> None:
     create_table(database_name, table_name)
     conn = sqlite3.connect(f"./database/{database_name}.db")
     cursor = conn.cursor()
-    cursor.execute(f"SELECT date FROM {table_name}")
+    cursor.execute(f"SELECT date FROM '{table_name}'")
     existing_data = {row[0] for row in cursor.fetchall()}
     df["date"] = df["date"].astype(str)
     filltered_data = [
@@ -42,7 +42,7 @@ def add_to_db(
     ]
     if filltered_data:
         cursor.executemany(
-            f"""INSERT INTO {table_name} (date, open, high, low, close, volume)
+            f"""INSERT INTO '{table_name}' (date, open, high, low, close, volume)
                            VALUES (?, ?, ?, ?, ?, ?)""",
             filltered_data,
         )
@@ -52,8 +52,8 @@ def add_to_db(
         last_date = max(row[0] for row in filltered_data)
 
         print(
-            f"{len(filltered_data)} data points added to {
-              table_name} table from: {first_date} to {last_date}"
+            f"""{len(filltered_data)} data points added to {
+              table_name} table from: {first_date} to {last_date}"""
         )
     else:
         print(f"No new data add to {table_name} table.")
@@ -61,17 +61,35 @@ def add_to_db(
     conn.close()
 
 
+async def split_path_to_create_db(path: Path):
+    path = path.parent / "exchange-fetched-data"
+    dfs = glob(
+        f"{path}/*.csv",
+    )
+    df_name = [Path(df).name for df in dfs]
+    symbol = list(map(lambda x: x.split("-")[0], df_name))
+    dataframe = list(map(lambda x: x.split("-")[1], df_name))
+    return list(zip(dfs, symbol, dataframe))
+
+
+async def async_ad_to_db(params) -> None:
+    for param in params:
+        path, symbol, timeframe = param
+        print(path, symbol, timeframe)
+        df = pd.read_csv(path)
+        add_to_db(df=df, database_name=symbol, table_name=timeframe)
+
+
 def remove_duplicates(database_name: str, table_name: str) -> None:
     conn = sqlite3.connect(f"./database/{database_name}.db")
     cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM {table_name}")
+    cursor.execute(f"SELECT * FROM '{table_name}'")
     before = cursor.fetchall()
     cursor.execute(
-        f"DELETE FROM {
-                   table_name} where id not in (SELECT MIN(id) FROM data GROUP BY date)"
+        f"""DELETE FROM '{table_name}' where id not in (SELECT MIN(id) FROM data GROUP BY date)"""
     )
     conn.commit()
-    cursor.execute(f"SELECT * FROM {table_name}")
+    cursor.execute(f"SELECT * FROM '{table_name}'")
     after = cursor.fetchall()
     conn.close()
     print(f"{len(before) - len(after)} rows deleted.")
